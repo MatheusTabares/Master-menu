14c8000a55ebd1e7d2d5939be32a6c7d96e60048
MatheusTabares <tabares.lopes@gmail>
2017-09-16 11:23:10 -0300

    adicionado pagina menu

--- Mastermenu/src/main/resources/public/js/controllers/app.config.js
+++ Mastermenu/src/main/resources/public/js/controllers/app.config.js
@@ -1,6 +1,10 @@
 angular.module('mastermenuModule', [ 'ngRoute', 'mastermenuControllers', 'mastermenuServices' ])
 		.config([ '$routeProvider', function($routeProvider) {
-			$routeProvider.when('/main', {
+			$routeProvider.when('/menu', {
+				templateUrl : '../../view/client/menu.html',
+				controller : 'MenuCtrl'
+			})
+			.when('/main', {
 				templateUrl : '../../view/client/main.html',
 				controller : 'MainCtrl'
 			})
